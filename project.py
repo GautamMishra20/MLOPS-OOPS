472b515b6cb35050d35cfd6b92f76c98ed106d62
Gautam Mishra <g20autammishra1234@gmail.com>
2026-06-04 17:24:01 +0530

    signup and signin completed

--- project.py
+++ project.py
@@ -14,14 +14,39 @@ class chatbook:
                            4. Press 4 to message a friend
                            5. Press any other key to exit""")
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
-            
+    
+    def signup(self):
+        email = input("enter your email here -> ")
+        password = input("setup your password here -> ")
+        self.username = email
+        self.password = password
+        print("You have signed up successfully !!")
+        print("\n")
+        self.menu()
+        
+    def signin(self):
+        if self.username == "" and self.password == "":
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            username = input("enter your email/username here -> ")
+            password = input("enter your password here -> ")
+            if self.username == username and  self.password == password:
+                print("you have signed in successfully !!")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials..")
+        print("\n")
+        self.menu()
+
+        
+        
 obj = chatbook()
